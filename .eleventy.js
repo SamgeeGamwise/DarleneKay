a40aa57e0ede224eac0ab3a9db3879a722f6c349
Samuel Krohn <sanikr@outlook.com>
2025-09-13 12:37:21 -0600

    prefix path

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,10 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("src/styles");
-
-  // Copy static assets only (images, fonts, etc.)
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
 
   return {
     dir: { input: "src", includes: "_includes", data: "_data", output: "_site" },
-    templateFormats: ["njk", "md", "html"]
+    templateFormats: ["njk", "md", "html"],
+    pathPrefix: "/",
   };
 };
