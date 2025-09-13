62bcc4318d5c41d9e0d72402c73718082fc5edc6
Samuel Krohn <sanikr@outlook.com>
2025-09-13 12:46:21 -0600

    Additional href updates

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,12 +3,20 @@ module.exports = {
   site_name: "Darlene Kay Painting",
   tagline: "Interior & exterior painting in the greater Denver area",
   nav: [
-    { href: "/DarleneKay/", text: "Home" },
-    { href: "/DarleneKay/residential-services/", text: "Residential" },
-    { href: "/DarleneKay/gallery/", text: "Gallery" },
-    { href: "/DarleneKay/news/", text: "News" },
-    { href: "/DarleneKay/testimonials/", text: "Testimonials" },
-    { href: "/DarleneKay/contact/", text: "Contact" }
+    { href: "/", text: "Home" },
+    { href: "/residential-services/", text: "Residential" },
+    { href: "/gallery/", text: "Gallery" },
+    { href: "/news/", text: "News" },
+    { href: "/testimonials/", text: "Testimonials" },
+    { href: "/contact/", text: "Contact" }
+  ],
+  footer: [
+    { href: "/interior-services-overview", text: "Interior Services" },
+    { href: "/exterior-services-overview", text: "Exterior Services" },
+    { href: "/commercial-services", text: "Commercial Services" },
+    { href: "/residential-services", text: "Residential Services" },
+    { href: "/contact", text: "Contact Us" },
+    { href: "/careers", text: "Careers" }
   ],
   year: new Date().getFullYear()
 };
