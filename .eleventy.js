dab71622d74c050cc4214d3ec52707d2a5ea7a8f
Samuel Krohn <sanikr@outlook.com>
2025-09-15 10:41:18 -0600

    Clean up footer, make mobile

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addWatchTarget("src/script"); // watch for changes
   eleventyConfig.addPassthroughCopy({ "src/script": "script" }); // copy to _site/assets/js
+  eleventyConfig.addPassthroughCopy({ "src/assets/fontawesome": "assets/fontawesome" });
+
 
   return {
     dir: { input: "src", includes: "_includes", data: "_data", output: "_site" },
