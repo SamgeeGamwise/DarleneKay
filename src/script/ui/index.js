8beffe82e1c0c17000ab814d11e4fa2d77c45a56
Samuel Krohn <sanikr@outlook.com>
2025-09-15 14:31:55 -0600

    Updates

--- src/script/ui/index.js
+++ src/script/ui/index.js
@@ -1,11 +1,19 @@
-document.getElementById("menuButton").addEventListener("click", () => {
+document.getElementById("menu-button").addEventListener("click", () => {
   document.getElementById("menu").classList.toggle("hidden");
+
+  const buttonIcon = document.getElementById("menu-button-icon");
+  buttonIcon.classList.toggle("fa-xmark");
+  buttonIcon.classList.toggle("fa-bars");
 });
 
 document.addEventListener("click", (e) => {
   const menu = document.getElementById("menu");
-  const button = document.getElementById("menuButton");
-  if (!menu.contains(e.target) && !button.contains(e.target)) {
+  const button = document.getElementById("menu-button");
+  const buttonIcon = document.getElementById("menu-button-icon");
+
+  if (!menu.contains(e.target) && !button.contains(e.target) && !menu.classList.contains("hidden")) {
     menu.classList.add("hidden");
+    buttonIcon.classList.toggle("fa-xmark");
+    buttonIcon.classList.toggle("fa-bars");
   }
 });
