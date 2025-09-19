16655fb2c2342e6ca28c7e0f49c02c9927de37d9
Samuel Krohn <sanikr@outlook.com>
2025-09-19 13:37:55 -0600

    Major changes

--- src/data/site.js
+++ src/data/site.js
@@ -2,27 +2,35 @@
 module.exports = {
   site_name_painting: "Darlene Kay Painting",
   site_name_roofing: "Darlene Kay Roofing and Gutters",
-  tagline: "Interior & exterior painting in the greater Denver area",
+  site_name: "Darlene Kay",
+  tagline: "Interior & exterior painting and roofing in the greater Denver area",
   nav: [
-    { href: "/about", text: "About Us"  },
-    { href: "/services", text: "Services", children: [
-      { href: "/residential-services/", text: "Residential" },
-      { href: "/commercial-services/", text: "Commercial" },
-    ]},
-    { href: "/gallery/", text: "Gallery" },
+    {
+      href: "/painting/",
+      text: "Painting",
+      children: [
+        { href: "/residential-services/", text: "Residential" },
+        { href: "/commercial-services/", text: "Commercial" },
+        { href: "/gallery/", text: "Gallery" }
+      ]
+    },
+    { href: "/roofing/", text: "Roofing" },
+    { href: "/service-areas/", text: "Service Areas" },
+    { href: "/about/", text: "About" },
     { href: "/news/", text: "News" },
     { href: "/testimonials/", text: "Testimonials" },
     { href: "/contact/", text: "Contact" }
   ],
   footer: [
-    { href: "/about", text: "About Us"  },
-    { href: "/commercial-services", text: "Commercial Services" },
-    { href: "/residential-services", text: "Residential Services" },
+    { href: "/painting/", text: "Painting" },
+    { href: "/roofing/", text: "Roofing" },
+    { href: "/residential-services/", text: "Residential" },
+    { href: "/commercial-services/", text: "Commercial" },
+    { href: "/service-areas/", text: "Service Areas" },
     { href: "/gallery/", text: "Gallery" },
     { href: "/news/", text: "News" },
     { href: "/testimonials/", text: "Testimonials" },
-    { href: "/contact", text: "Contact Us" },
-    { href: "/careers", text: "Careers" }
+    { href: "/contact/", text: "Contact" }
   ],
   reviews: [
     { reviewer: "Dina E", stars: 5, source: "Google Review", link: "https://share.google/BN6OvaRRcBtMPIXoR", text: 'We had a wonderful experience hiring this company to paint several rooms in our townhome. The owners were knowledgeable and provided excellent customer service and the painters were very talented. The project was completed on time, and we couldn\'t be happier.' },
@@ -31,7 +39,7 @@ module.exports = {
     { reviewer: "William M", source: "BBB Review", link: "https://www.bbb.org/us/co/aurora/profile/painting-contractors/darlene-kay-llc-1296-90246504/customer-reviews", stars: 5, text: 'What a great company. They are the best in the business. I will give them 10 stars for their work, customer service, cost and completion of work in a timely manner. What a great group of people this company has. I will highly recommend them for your next paint job.' },
     { reviewer: "Philip T", source: "Google Review", link: "https://share.google/jNRc2eBUMlCqL9RFS", stars: 5, text: 'The project was handled with complete professionalism. They did exactly what work was quoted for exactly what was requested for that work. Very clean job site and the men were considerate and attentive to our needs. I would use these guys again and refer them to all my friends and neighbors - Thanks again guys.' },
     { reviewer: "Madeleine S", source: "BBB Review", link: "https://www.bbb.org/us/co/aurora/profile/painting-contractors/darlene-kay-llc-1296-90246504/customer-reviews", stars: 5, text: 'The Thomases are so kind, professional, and most importantly, amazingly skilled at what they do. They have so much integrity and will only offer the best quality, best advice, and best work. We just moved into a new house and they helped us matched existing paint and baseboards. Very clean lines, perfect color matching, texture matching, everything perfect. We will absolutely use them next time we need interior paint, and are planning on using them for exterior soon!' },
-    { reviewer: "Russ W", source: "Google Review", link: "https://share.google/1pGYZVO4q6efmgtqz", stars: 5, text: 'Hands down the best painting company I’ve ever had the pleasure of working with. Roy and his crew did outstanding work painting the interior of our home. They showed up on time, left on time, and did high quality work. Roy is an outstanding communicator and made sure that we were happy with every detail of the job. I’ve hired this company on various occasions and always had a good experience.' },
+    { reviewer: "Russ W", source: "Google Review", link: "https://share.google/1pGYZVO4q6efmgtqz", stars: 5, text: 'Hands down the best painting company I\'ve ever had the pleasure of working with. Roy and his crew did outstanding work painting the interior of our home. They showed up on time, left on time, and did high quality work. Roy is an outstanding communicator and made sure that we were happy with every detail of the job. I\'ve hired this company on various occasions and always had a good experience.' }
   ],
   year: new Date().getFullYear()
 };
